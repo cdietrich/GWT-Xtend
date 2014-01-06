3fea217ce2d59cfeb96abd9b7b187100dbf42592
GilraenBurland <selenalowell@outlook.de>
2014-01-06 21:47:50 +0100

    a

--- gwxtend/src/main/xtend-gen/org/gwxtend/annotations/GwtService.java
+++
@@ -1,9 +0,0 @@
-package org.gwxtend.annotations;
-
-import org.eclipse.xtend.lib.macro.Active;
-import org.gwxtend.annotations.GwtServiceProcessor;
-
-@Active(GwtServiceProcessor.class)
-public @interface GwtService {
-  public String moduleName();
-}
--- gwxtend/src/main/xtend-gen/org/gwxtend/server/GreetingServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package org.gwxtend.server;
-
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-
-@SuppressWarnings("all")
-public class GreetingServiceImpl extends RemoteServiceServlet {
-}
